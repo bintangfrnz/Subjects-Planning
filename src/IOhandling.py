2ec3daa28482c489589e2512979f708bee770404
frnzoshi-tech <75726616+frnzoshi-tech@users.noreply.github.com>
2021-05-30 08:46:16 +0700

    add display result func

--- src/IOhandling.py
+++ src/IOhandling.py
@@ -8,3 +8,24 @@ def getResource(filename):
             line = f.readline()
     return graph
 
+def SwitchToRoman(num):
+    semester = {
+        1: "I",
+        2: "II",
+        3: "III",
+        4: "IV",
+        5: "V",
+        6: "VI",
+        7: "VII",
+        8: "VIII"
+    }
+    return semester.get(num, "Only handle 8 semesters")
+
+def ShowResult(results):
+    print("--- Result ---")
+    for i in range(len(results)):
+        if i > 7:
+            break
+        
+        subject = ', '.join(results[i])
+        print(f'Semester {SwitchToRoman(i+1)}\t: {subject}')
